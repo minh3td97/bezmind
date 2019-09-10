1ac614314bf02f388090aeb98a17db248f74b81e
minhnguyen <minh.nguyenquang.ncc@gmail.com>
2019-09-11 02:20:15 +0900

    aaa

--- app/src/main/java/com/example/android/bezmind/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/android/bezmind/welcome/WelcomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.android.bezmind.welcome
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
@@ -30,39 +32,35 @@ class WelcomeFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-       makeGameNameMove()
+        makeGameNameMove()
+        setupSpots()
     }
+
     private var gameNameThread: GameNameThread? = null
+    private var spotThread: SpotThread? = null
+    val uiChangeHandler = Handler()
+
     private fun makeGameNameMove(){
         gameNameThread = GameNameThread().apply {
             this.start()
         }
     }
-
     inner class GameNameThread: Thread(){
         private var isRunning = true
-        private val unitTime = 1000L
-        private val limitRotation = 4.0f
+        private val unitTime = 600L
         private var rotateTime = 0L
         override fun run() {
             rotateTime = SystemClock.uptimeMillis()
-            var buttonTextSize = binding.startButton.textSize
             var currentTime = 0L
             while (isRunning && !Thread.interrupted()){
                 currentTime = SystemClock.uptimeMillis() - rotateTime
-                var time = 1.0f
 
-                if(currentTime >= 2 * unitTime){
-                    rotateTime = SystemClock.uptimeMillis()
-                    time = -1.0f
-                } else if (currentTime >= unitTime){
-                    time = 1 - currentTime.toFloat()/unitTime.toFloat()
-                } else {
-                    time = currentTime.toFloat()/unitTime.toFloat()
+                if(currentTime >= unitTime){
+                    var time = (-3..3).random()
+                    binding.gameName.rotation =  time.toFloat()
+                    rotateTime = 0L
                 }
 
-                binding.gameName.rotation = limitRotation * time
-                binding.startButton.textSize = time
                 try{
                     sleep(25)
                 }catch (e: InterruptedException) {
@@ -70,6 +68,66 @@ class WelcomeFragment: Fragment() {
                 }
             }
 
+        }
+
+        override fun interrupt() {
+            isRunning = false
+            binding.gameName.rotation =  0f
+        }
+    }
+    private fun setupSpots(){
+        spotThread = SpotThread().apply {
+            this.start()
+        }
+    }
+    inner class SpotThread(
+    ): Thread(){
+        private var isRunning = true
+        private var maxScaleTime = 500L
+        private var startScaleTime = 0L
+        override fun run() {
+            startScaleTime = SystemClock.uptimeMillis()
+
+            var currentTime = 0L
+            var visibleStatus = View.VISIBLE
+            var isShow = true
+            while (isRunning && !Thread.interrupted()){
+                currentTime = SystemClock.uptimeMillis() - startScaleTime
+                var index = (currentTime / maxScaleTime).toInt()
+                if(index > 6){
+                    if (isShow){
+                        visibleStatus = View.INVISIBLE
+                        isShow = false
+                    } else {
+                        visibleStatus = View.VISIBLE
+                        isShow = true
+                    }
+
+                    currentTime = 0L
+                    startScaleTime = SystemClock.uptimeMillis()
+                    index = 0
+                }
+
+                uiChangeHandler.post(){
+                    getSpot(index).visibility = visibleStatus
+                }
+
+            }
+        }
+        private fun getSpot(index: Int) : View{
+            when(index){
+                0 -> return binding.spot1
+                1 -> return binding.spot2
+                2 -> return binding.spot3
+                3 -> return binding.spot4
+                4 -> return binding.spot5
+                5 -> return binding.spot6
+                else -> return binding.spot7
+            }
+        }
+
+        override fun interrupt() {
+            isRunning = false
         }
     }
 }
